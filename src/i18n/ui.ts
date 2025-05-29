fdbd726586362173644fdfb8cc441fc178a8b18c
Yuzu Chen <yuzu.c@ikigai.team>
2025-05-29 13:21:21 +0800

    feat: use i18n ui for nav instead of using config.nav

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,11 +11,13 @@ export const ui = {
   en: {
     'nav.home': 'Home',
     'nav.blog': 'Blog',
+    'nav.about': 'About',
     'nav.twitter': 'Twitter',
   },
   'zh-TW': {
     'nav.home': '首頁',
     'nav.blog': '部落格',
+    'nav.about': '關於',
     // 'nav.twitter': 'Twitter',
   },
 } as const;
